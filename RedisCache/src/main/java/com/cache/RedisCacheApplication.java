a21b2b8f402a7af65e904274444807a78d7fc4cb
The-Quantum-Boy <dhanorkar.sumit.code@gmail.com>
2024-07-20 16:06:06 +0530

    some change

--- RedisCache/src/main/java/com/cache/RedisCacheApplication.java
+++ RedisCache/src/main/java/com/cache/RedisCacheApplication.java
@@ -3,11 +3,17 @@ package com.cache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class RedisCacheApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisCacheApplication.class, args);
+
+		int[] arr={1,2,2};
 	}
+	
 
 }
